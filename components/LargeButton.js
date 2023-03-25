bc6e5f5d70f41a044f7434489ea1ad2e96bed19c
solch4 <sol.maldonado.0209@gmail.com>
2023-03-25 00:53:15 -0300

    active styles for buttons added

--- components/LargeButton.js
+++ components/LargeButton.js
@@ -2,7 +2,7 @@ export default function LargeButton({ handleClick, children }) {
   return (
     <button
       onClick={handleClick}
-      className="px-8 py-4 rounded-lg bg-primary-100 hover:bg-primary-200 transition-[3s]"
+      className="px-8 py-4 rounded-lg bg-primary-100 hover:bg-primary-200 active:scale-95 transition"
     >
       {children}
     </button>
--- components/SmallButton.js
+++ components/SmallButton.js
@@ -4,7 +4,7 @@ export default function SmallButton({ link, children }) {
       href={link}
       target="_blank"
       rel="noreferrer"
-      className="px-6 py-2 rounded-lg bg-primary-100 hover:bg-primary-200 transition-[3s]"
+      className="px-6 py-2 rounded-lg bg-primary-100 hover:bg-primary-200 active:scale-95 transition"
     >
       {children}
     </a>
