7bba5784648dfac1583a38eb6023fa2a4029c8a7
solch4 <sol.maldonado.0209@gmail.com>
2023-07-24 20:25:55 -0300

    styles variables added

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,5 +1,7 @@
 export const styles = {
-  container: "w-full max-w-[1440px] mx-auto px-4 py-8 md:px-6 lg:px-24 md:py-[5.5rem]",
-  innerWidth: "w-full max-w-[1440px] mx-auto px-4 md:px-6 lg:px-24",
+  container: "w-full max-w-[1440px] mx-auto px-4 py-8 md:px-6 xl:px-24 md:py-[5.5rem]",
+  innerWidth: "w-full max-w-[1440px] mx-auto px-4 md:px-6 xl:px-24",
   verticalFlow: "flex flex-col gap-6 md:gap-12",
+  label: "text-neutral-600 absolute duration-150 transform -translate-y-3 scale-75 top-4 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3",
+  input: "w-full bg-terciary-50 px-4 pt-6 pb-1 focus:outline-none focus:ring-0 peer",
 };
